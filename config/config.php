0c5e3169bc28fa15ac1aa929702fe3e686724337
George Birnbaum <gb@glamorise.com>
2020-03-24 09:56:01 -0400

    add phpunit.xml and remove .idea

--- config/config.php
+++ config/config.php
@@ -4,5 +4,27 @@
  * You can place your custom package configuration in here.
  */
 return [
+    'store' => [
+        'store1' => [
+            'merchantId' => '',
+            'marketplaceId' => '',
+            'keyId' => '',
+            'secretKey' => '',
+            'authToken' => '',
+            'amazonServiceUrl' => 'https://mws-eu.amazonservices.com/',
+            /** Optional settings for SOCKS5 proxy
+             *
+            'proxy_info' => [
+            'ip' => '127.0.0.1',
+            'port' => 8080,
+            'user_pwd' => 'user:password',
+            ],
+             */
+        ]
+    ],
 
+    // Default service URL
+    'AMAZON_SERVICE_URL' => 'https://mws.amazonservices.com/',
+
+    'muteLog' => false
 ];
