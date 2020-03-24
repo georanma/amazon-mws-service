d4c90a52d708debb1b196b6fcd14528d729921df
George Birnbaum <gb@glamorise.com>
2020-03-24 10:46:12 -0400

    remove unnecessary code.

--- src/AmazonMwsServiceProvider.php
+++ src/AmazonMwsServiceProvider.php
@@ -11,33 +11,10 @@ class AmazonMwsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'amazon-mws-service');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'amazon-mws-service');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('amazon-mws-service.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/amazon-mws-service'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/amazon-mws-service'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/amazon-mws-service'),
-            ], 'lang');*/
 
             // Registering package commands.
             // $this->commands([]);
